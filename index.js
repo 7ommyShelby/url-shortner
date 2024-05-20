2eac554653690269a9fa61ece86cc4fb88e81d4a
7ommyShelby <aryaalsaher@gmail.com>
2024-05-20 23:33:34 +0530

    first

---
+++ index.js
@@ -0,0 +1,69 @@
+// import express from "express";
+// import { nanoid } from "nanoid";
+
+const express = require("express");
+const uniqid = require('generate-unique-id');
+const fs = require("fs")
+
+const app = express();
+
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname +'form.html')
+})
+
+app.post('/short-url', (req, res) => {
+
+    console.log(req.body.url);
+
+    try {
+
+        const data = fs.readFileSync('urlmap.json', { encoding: "utf-8" });
+        // console.log("test", data.length);
+        let urldata
+
+        if (data == '') {
+            urldata = {}
+        } else {
+            urldata = JSON.parse(data);
+        }
+
+        const shorturl = uniqid({
+            length: 8,
+            useLetters: true,
+            useNumbers: true
+        })
+
+        urldata[shorturl] = req.body.url
+
+        fs.writeFileSync("urlmap.json", JSON.stringify(urldata));
+
+    } catch (error) {
+        console.log(error);
+    }
+
+    res.json({
+        "status": true,
+        "message": "Url Shortner Application",
+        "data": `http://localhost:8000/${shorturl}`,
+    })
+})
+
+
+app.get("/:shorturl", (req, res) => {
+
+    const file = fs.readFileSync("urlmap.json", { encoding: "utf-8" });
+    const filejson = JSON.parse(file);
+    const surl = req.params.shorturl;
+    const longurl = filejson[surl];
+
+    res.redirect(longurl);
+
+
+
+})
+
+app.listen(8000, () => {
+    console.log("server up and running");
+})
